5b09c6452a839d97813121272051bf428919f59b
ivandev45 <ivan.dev45@gmail.com>
2017-06-07 15:39:34 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -5,7 +5,9 @@
 const config = {
     watsonApiKey : '98627e88a32c1b7c730e7140d0e2a3dda2b70243',
     telegramToken : '346855979:AAFzYCtm5HWagYnQfsVNBjUGLlB_VoKfPbg',
-    classifierId : 'Dog_1456209710',
+    classifierId : "Dog_1456209710",
+    version : 'v3',
+    version_date: '2016-05-20',
     downloadFileUri : (token, path) => {
         return 'https://api.telegram.org/file/bot' + token + '/' + path;
     }
